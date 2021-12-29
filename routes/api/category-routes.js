8c1f742a107c26a9f749e942adbf56cc2a388782
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-12-29 00:46:16 -0600

    Bussin these routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,7 +44,6 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
       where: {
@@ -62,8 +61,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!categoryData) {
+      res.status(400).json({ message: 'No category found with that id!'})
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
